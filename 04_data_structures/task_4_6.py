0e2f427082522dd31c0669ce99ee26747333a455
Chung Tran <bigearsenal@gmail.com>
2018-10-30 15:59:39 +0300

    4.6 completed

--- 04_data_structures/task_4_6.py
+++ 04_data_structures/task_4_6.py
@@ -14,4 +14,14 @@ Outbound Interface:    FastEthernet0/0
 
 '''
 
-ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_route = 'O 10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_route = ospf_route.replace('O','OSPF')
+
+RESULT = ospf_route.split(' ')
+
+print('Protocol: ', RESULT[0])
+print('Prefix: ', RESULT[1])
+print('AD/Metric: ', RESULT[2])
+print('Next-Hop: ', RESULT[4])
+print('Last update: ', RESULT[5])
+print('Outbound Interface: ', RESULT[6])
